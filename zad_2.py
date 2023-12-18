1c745109e3665ffa3ce3ad682ccab0abf7b68c27
mlinsk <147390681+mlinskyy@users.noreply.github.com>
2023-12-18 10:52:56 +0100

    lecimy po milion

---
+++ zad_2.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+liczba1 = int(input("Podaj pierwszą liczbę całkowitą: "))
+liczba2 = int(input("Podaj drugą liczbę całkowitą: "))
+
+wynik = liczba1 * liczba2
+ 
+print("Wynik mnożenia: {}".format(wynik))
