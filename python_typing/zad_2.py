9b3bbd6936059b0874bf0ffd08179c8a6285b10b
mlinsk <147390681+mlinskyy@users.noreply.github.com>
2023-12-18 10:50:27 +0100

    Delete python_typing directory

--- python_typing/zad_2.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-liczba1 = int(input("Podaj pierwszą liczbę całkowitą: "))
-liczba2 = int(input("Podaj drugą liczbę całkowitą: "))
-
-wynik = liczba1 * liczba2
- 
-print("Wynik mnożenia: {}".format(wynik))
