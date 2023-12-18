835383b4cd706b99b18e5ba06675449b3b6a55d8
mlinsk <147390681+mlinskyy@users.noreply.github.com>
2023-12-18 10:54:00 +0100

    Delete python_basic directory

--- python_basic/zad 2b1.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-lista = [1, 2, 3, 4, 20]
-
-wynik = []
-for liczba in lista:
-        wynik.append(liczba * 2)
-
-print(wynik)
--- python_basic/zad 2d.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-lista_liczb = list(range(1, 11))
-
-
-for i in range(0, len(lista_liczb), 2):
-        print(lista_liczb[i])
-
-
